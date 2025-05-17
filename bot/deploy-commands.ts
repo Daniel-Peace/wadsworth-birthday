4c209ebf1b241df6c0b88cf94a10770394eaef2f
Daniel Peace <danielpeace805@gmail.com>
2025-05-17 09:11:24 -0700

    Got a basic version of the discord bot running

---
+++ bot/deploy-commands.ts
@@ -0,0 +1,34 @@
+import { REST, Routes } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { token, clientId } from "./botConfig.json";
+
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = await import(filePath);
+  if ("data" in command && "execute" in command) {
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(clientId), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
---
+++ bot/index.ts
@@ -0,0 +1,46 @@
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { token } from "./botConfig.json";
+import fs from "fs";
+import path from "path";
+import type { Command } from "./types/Command";
+
+// creating collection for commands
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }) as Client & {
+  commands: Collection<string, Command>;
+};
+client.commands = new Collection();
+
+// creating path to the commands directory
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+
+// importing each command
+for (const file of commandFiles) {
+  const command = await import(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+  console.log(`Logged in as ${client.user!.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    await interaction.reply({
+      content: "There was an error!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(token);
